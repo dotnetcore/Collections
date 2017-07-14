7414f9aabbbb8a8fb8e514ba833802aca11e7de9
Lemon <liuhaoyang1221@hotmail.com>
2017-07-14 23:46:23 +0800

    添加Benchmark

---
+++ performance/DotNetCore.Collections.Paginable.Benchmarks/PaginableBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using DotNetCore.Collections.Paginable;
+using ABPaginableCollections = PaginableCollections;
+
+namespace DotNetCore.Collections.Paginable.Benchmarks
+{
+    [MaxColumn, MinColumn]
+    public class PaginableBenchmark
+    {
+        private readonly IEnumerable<int> _list;
+        public PaginableBenchmark()
+        {
+            _list = Enumerable.Range(0, 10000000);
+        }
+
+        [Benchmark]
+        public int DotNetCoreCollectionEnumerable()
+        {
+            var paginable = _list.GetPage(15, 50);
+            return paginable.TotalPageCount;
+        }
+
+        [Benchmark]
+        public int ABPaginableCollectionEnumerable()
+        {
+            var paginable = ABPaginableCollections.EnumerableExtensions.ToPaginable(_list, 15, 50);
+            return paginable.Count;
+        }
+
+        [Benchmark]
+        public int DotNetCoreCollectionQueryable()
+        {
+            var paginable = _list.AsQueryable().GetPage(15, 50);
+            return paginable.TotalPageCount;
+        }
+
+        [Benchmark]
+        public int ABPaginableCollectionQueryable()
+        {
+            var paginable = ABPaginableCollections.PaginableExtensions.ToPaginable(_list.AsQueryable(), 15, 50);
+            return paginable.Count;
+        }
+    }
+}
---
+++ performance/DotNetCore.Collections.Paginable.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Running;
+
+namespace DotNetCore.Collections.Paginable.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<PaginableBenchmark>();
+        }
+    }
+}
